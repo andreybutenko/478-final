da23d31eb89c8009835690c6389edbc331ed4198
Andrey Butenko <ghostmancer@gmail.com>
2020-05-12 17:04:03 -0700

    Add US Promise Zones and Risk Factors data

---
+++ preprocessing/kaggle-covid-19-risk-factors-preprocessing.R
@@ -0,0 +1,8 @@
+# US COVID-19 Risk Factors Assessment Data
+# Dataset of COVID-19 risk factors by state.
+# https://www.kaggle.com/jtourkis/covid19-us-major-city-density-data/data#
+
+raw_data <- read.csv('../data/raw/Kaggle - US COVID-19 Risk Factors Assessment Data.csv',
+                     stringsAsFactors = F)
+
+# TODO: Preprocessing based on what we want to explore in the risk factors data.
