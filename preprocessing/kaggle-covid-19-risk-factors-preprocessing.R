ed138f37ac1133e50bca886e62f20f4132d10f4e
Shivank Mistry <shivank.mistry@gmail.com>
2020-05-14 14:36:55 -0600

    processed commuter data

--- preprocessing/kaggle-covid-19-risk-factors-preprocessing.R
+++ preprocessing/kaggle-covid-19-risk-factors-preprocessing.R
@@ -6,3 +6,32 @@ raw_data <- read.csv('../data/raw/Kaggle - US COVID-19 Risk Factors Assessment D
                      stringsAsFactors = F)
 
 # TODO: Preprocessing based on what we want to explore in the risk factors data.
+
+library(dplyr)
+library(readxl)
+
+# processing data for commuter and-Covid related information
+commuter_clean_data <- raw_data %>% 
+  select(REGION, 
+         Area, 
+         Covid_Positive_4_4,
+         Covid_Death_4_4, 
+         CENSUS_2010_TOTAL_POPULATION,
+         COMMUTE_Census_Worker_Drive_To_Work_Rate,
+         COMMUTE_Census_Worker_Carpool_To_Work_Rate,
+         COMMUTE_Census_Worker_Public_Transportation_Rate,
+         COMMUTE_Census_Worker_Walk_Rate,
+         COMMUTE_Census_Worker_Taxicab_Motorcycle_Bicycle_Other_Rate,
+         COMMUTE_Census_Work_At_Home_Rate) %>% 
+  rename(Region = REGION, 
+          Total_Population_2010 = CENSUS_2010_TOTAL_POPULATION,
+          Drive_to_work_rate = COMMUTE_Census_Worker_Drive_To_Work_Rate,
+          Carpool_to_work_rate = COMMUTE_Census_Worker_Carpool_To_Work_Rate,
+          Public_transportation_rate = COMMUTE_Census_Worker_Public_Transportation_Rate,
+          Walk_to_work_rate = COMMUTE_Census_Worker_Walk_Rate,
+          Other_commuter_rate = COMMUTE_Census_Worker_Taxicab_Motorcycle_Bicycle_Other_Rate,
+          work_from_home_rate = COMMUTE_Census_Work_At_Home_Rate)
+write.csv(commuter_clean_data,
+          '../data/prepped/covid-19-commuter-data-prepped.csv',
+          row.names = F)
+
