0e0618a0d6c0d50553da60f7b69c07e6b7771291
Shivank Mistry <shivank.mistry@gmail.com>
2020-05-28 17:30:05 -0600

    finished preprocessing

--- preprocessing/NYC-public-transit-preprocessing.R
+++ preprocessing/NYC-public-transit-preprocessing.R
@@ -5,5 +5,18 @@ library(plotly)
 library(stringr)
 library(readxl)
 
+# load raw data
+nyc_subway_loc <- read.csv('./data/raw/NYC_Subway_Station_Location.csv', stringsAsFactors = F)
+
+nyc_subway_traffic <- read_excel('./data/raw/MTA_SubwayTraffic.xlsx')
+  
 nyc_covid <- read.csv('./data/raw/NYC_Covid_by_ZipCode.csv', stringsAsFactors = F)
+
+# clean traffic volume data
+subway_traffic_clean <- nyc_subway_traffic %>% 
+  mutate(na = is.na(`2018`)) %>% 
+  filter(na == F)
+
+write.csv(nyc_covid, './data/prepped/nyc_covid_data', row.names = F)
+write.csv(subway_traffic_clean, './data/prepped/subway_Traffic_Data.csv', row.names = F)
 
